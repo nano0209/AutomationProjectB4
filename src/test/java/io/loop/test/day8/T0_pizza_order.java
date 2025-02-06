cf2354f644a7b946fda3a3faa2bfb294de0c3f4b
naima <naimamus36@gmail.com>
2025-02-06 18:58:16 -0500

    day 8

---
+++ src/test/java/io/loop/test/day8/T0_pizza_order.java
@@ -0,0 +1,98 @@
+package io.loop.test.day8;
+
+import io.loop.test.utilities.PizzaOrderWebTableUtils;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.loop.test.utilities.Driver.getDriver;
+import static org.testng.Assert.assertEquals;
+
+public class T0_pizza_order {
+
+    @BeforeMethod
+    public void url (){getDriver().get("https://loopcamp.vercel.app/web-tables.html");
+    }
+
+    @Test
+    public void test_pizza_type(){
+        String name = "Alexandra Gray";
+        String expectedPizzaType = "Thin Crust";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), name, "Pizza type"), expectedPizzaType);
+    }
+
+
+    @Test
+    public void test_amount(){
+        String name = "John Doe";
+        String expectedAccount = "3";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), name, "Amount"), expectedAccount);
+
+    }
+    @Test
+    public void test_date(){
+        //String name = "Bob Martin";
+        String expectedDate = "12/31/2021";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(),"Bob Martin", "Date" ), expectedDate);
+
+    }
+
+    @Test
+    public void test_street(){
+        //String name = "Robert Baratheon";
+        String expectedStreet = "29, Sanderson Ave";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(),"Robert Baratheon", "Street"),expectedStreet);
+
+    }
+
+    @Test
+    public void test_city(){
+        //String name = "Ned Stark";
+        String expectedCity = "Newcastle";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), "Ned Stark", "City" ), expectedCity);
+
+
+    }
+
+    @Test
+    public void test_state(){
+        // String name = "Stewart Dawidson";
+        String expectedState = "Canada";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(),"Stewart Dawidson", "State"),expectedState);
+
+    }
+
+    @Test
+    public void test_zip(){
+        //String name = "Samuel Jackson";
+        String expectedZip = "53665";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), "Samuel Jackson", "Zip"),expectedZip);
+
+    }
+
+    @Test
+    public void test_card(){
+        //String name = "Robert Baratheon";
+        String expectedCard = "MasterCard";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(),"Robert Baratheon", "Card"),expectedCard);
+
+    }
+
+    @Test
+    public void test_card_number(){
+        //String name = "Alexandra Gray";
+        String expectedCardNumber = "321456789012";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), "Alexandra Gray", "Card Number"),expectedCardNumber);
+
+    }
+
+    @Test
+    public void test_exp(){
+        //String name = "John Doe";
+        String expectedExp = "01/23";
+        assertEquals(PizzaOrderWebTableUtils.returnAnyFieldValue(getDriver(), "John Doe", "Exp" ),expectedExp);
+
+    }
+
+
+
+}
